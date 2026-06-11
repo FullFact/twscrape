b7ea01924845ec15ff49856e0559005df836ac30
nmenezes0 <nemenezes@hotmail.com>
2026-06-11 16:01:00 +0100

    chore: revert the correctness change, keep deviations from the package minimal

--- twscrape/xclid.py
+++ twscrape/xclid.py
@@ -284,17 +284,8 @@ def parse_anim_arr(soup: bs4.BeautifulSoup, vk_bytes: list[int]) -> list[list[fl
         raise Exception("Couldn't get XClientTxId animation array")
 
     idx = vk_bytes[5] % len(els)
-    # Locate the first cubic bezier command rather than skipping a fixed 9 chars,
-    # since the M command length varies and a wrong offset truncates the first segment.
-    d_str = els[idx].upper()
-    c_start = d_str.find("C")
-    if c_start < 0:
-        raise Exception("Couldn't get XClientTxId animation path")
-    dat = d_str[c_start + 1:].split("C")
+    dat = els[idx][9:].split("C")
     arr = [list(map(float, re.sub(r"[^\d]+", " ", x).split())) for x in dat]
-    arr = [x for x in arr if x]
-    if not arr:
-        raise Exception("Couldn't parse XClientTxId animation frames")
     return arr
 
 
